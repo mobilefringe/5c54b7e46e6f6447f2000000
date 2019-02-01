858ddafd6899eaffca7daee3c975cf49f0485472
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-01 16:27:48 -0500

    New Sudbury Centre 1.0  /social_links.vue

---
+++ social_links.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <a href="https://www.facebook.com/NewSudburyCentre" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
+        <a href="https://www.twitter.com/EYTCshops" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
+         
+    </div>
+    
+</template>
+<style>
+    .lb-data .lb-number, .lb-nav {
+        display:none!important;
+    }
+</style>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
