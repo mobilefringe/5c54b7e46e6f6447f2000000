5190ac8cebcc1995b32856553c3de1c316423d1a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-01 16:28:21 -0500

    New Sudbury Centre 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -2,7 +2,7 @@
     <div>
         <a href="https://www.facebook.com/NewSudburyCentre" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
         <a href="https://www.twitter.com/newsudbury" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
-        <a href="https://www.instagram.com/newsudburycentre/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
+        <a href="https://www.instagram.com/newsudburycentre/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
     </div>
     
 </template>
