0cabf910c95841506372d32dda503dc5f5e97487
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-04 16:46:29 -0500

    New Sudbury Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -130,11 +130,11 @@
                     var features = this.$store.state.feature_items;
                     _.forEach(features, function(value, key) {
                       
-                        if( _.includes([0,5], key) ) {
+                        if( _.includes([0,4], key) ) {
                             value.masonry_class = "grid-item--height2";
                             value.image_url = "https://picsum.photos/570/1140?image=98"+key;
                         }
-                        else if ( _.includes([2,4], key) ){
+                        else if ( _.includes([2,5], key) ){
                             value.masonry_class = "grid-item--width2";
                             value.image_url = "https://picsum.photos/1140/570?image=97"+key;
                         }
