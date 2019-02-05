f70abd8b97810f1d4712af80169f73767d2d6acb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-05 17:04:19 -0500

    New Sudbury Centre 1.0  /main.js

--- main.js
+++ main.js
@@ -192,7 +192,12 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                         menu.show_sub_menu = false;
                     }
                 });
-            }
+            },
+            shareURL(page,slug){
+                console.log("page, slug", page, slug)
+                var share_url= "http://www.langhamsquare.ca/" + page + "/" + slug;
+                return share_url;
+            },
         },
         destroyed: function() {
             window.removeEventListener('scroll', this.enableStickyNavbar);
