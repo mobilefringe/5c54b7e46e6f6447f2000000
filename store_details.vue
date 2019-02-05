2f5ec9b216f5ad317569998117ca0ba607f170f1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-05 17:47:07 -0500

    New Sudbury Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -15,6 +15,7 @@
 						<!--<img v-lazy="currentStore.store_front_url_abs" class="image"/>-->
 						<div v-if="currentStore.no_store_logo" class="store_details_image center-block">
                             <div class="no_logo">
+                                <img class="store_img" src="//www.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
                                 <h2 class="store_details_name">{{ currentStore.name }}</h2>
                             </div>    
                         </div>
