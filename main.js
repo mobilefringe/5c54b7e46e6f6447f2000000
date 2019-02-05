c259503382ba9ec77d86ff3bcb0101e70d14d2b3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-05 15:39:16 -0500

    New Sudbury Centre 1.0  /main.js

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
             return {
                 dataLoaded: false,
                 show_mobile_menu: false,
-                property_logo: "//codecloud.cdn.speedyrails.net/sites/5c17f84d6e6f643522450000/image/png/1545071987721/logo.png",
+                property_logo: "//codecloud.cdn.speedyrails.net/sites/5c54b7e46e6f6447f2000000/image/png/1549056394014/new_sudbury_transparent.png",
                 menu_items: MenuItems,
                 search_result : null,
                 suggestionAttribute: 'name',
