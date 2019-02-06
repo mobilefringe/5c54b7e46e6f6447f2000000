3b70fc3e153549daeb470f1c30534821c0ca2547
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-06 12:07:08 -0500

    New Sudbury Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -56,7 +56,7 @@
 					</div>
 					<div class="store_promo_container" v-if="promotions.length > 0">
 						<div class="promo_container_title text-left caps"></div>
-						<h4 v-if="currentStore.rich_description" class="store_dets_title caps margin_30">{{$t("promos_page.promotions")}}</h4>
+						<h4 class="store_dets_title caps margin_30">{{$t("promos_page.promotions")}}</h4>
 						<div class="row store_promo_dets text-left" v-for="promo in promotions">
 							<div class="col-sm-6 no_padding" >
 								<div class="promo_div_image text-center">
