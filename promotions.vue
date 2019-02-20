b2689ab7a356f9b081ebb09fb7a5fdbcaaa4d858
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-20 14:59:53 -0500

    New Sudbury Centre 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -75,7 +75,7 @@
 			<!--	</div>-->
 			<!--</div>-->
 			
-			<div class="show_more"  v-if="promos && showMore <= promos.length">
+			<div class="show_more"  v-if="promos && showMore < promos.length">
               <div
                 class="pointer"
                 @click="loadMoreItems()"
@@ -176,6 +176,23 @@
                     var num = this.showMore + this.incrementBy;
                     this.showMore = num;
                   }
+                },
+                checkImageURL(value) {
+                    if (_.includes(value.image_url, "missing")) {
+                        if (value.store === null || value.store === undefined) {
+                            return "https://www.mallmaverick.com/system/site_images/photos/000/044/637/original/default_logo.png?1539618404";
+                        } else if (
+                            value.store != null &&
+                            value.store != undefined &&
+                            _.includes(value.store.store_front_url_abs, "missing")
+                        ) {
+                            return "https://www.mallmaverick.com/system/site_images/photos/000/044/637/original/default_logo.png?1539618404";
+                        } else {
+                            return value.store.store_front_url_abs;
+                        }
+                    } else {
+                        return value.promo_image_url_abs;
+                    }
                 }
             }
         });
