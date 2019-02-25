e30376123b27aed5c0395f4888c204317565479c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-25 11:01:31 -0500

    New Sudbury Centre 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -213,6 +213,23 @@
                     var num = this.showMore + this.incrementBy;
                     this.showMore = num;
                   }
+                },
+                checkImageURL(value) {
+                    if (_.includes(value.image_url, "missing")) {
+                        if (value.store === null || value.store === undefined) {
+                            return this.property.default_logo_url;
+                        } else if (
+                            value.store != null &&
+                            value.store != undefined &&
+                            _.includes(value.store.store_front_url_abs, "missing")
+                        ) {
+                            return null;
+                        } else {
+                            return value.store.store_front_url_abs;
+                        }
+                    } else {
+                        return value.promo_image_url_abs;
+                    }
                 }
             }
         });
