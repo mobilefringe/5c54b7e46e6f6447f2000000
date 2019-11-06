32ac48d3b567ae6a97bbab8ba9e706f164cf2780
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-06 15:55:17 -0500

    New Sudbury Centre 1.0  /main.js

--- main.js
+++ main.js
@@ -165,7 +165,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
                     let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores"), this.$store.dispatch("getData", "promotions"), this.$store.dispatch("getData", "events"),this.$store.dispatch("getData", "jobs"),this.$store.dispatch("getData", "repos")]);
-                    await Promise.all([this.$store.dispatch("LOAD_META_DATA")]);
+                    await Promise.all([this.$store.dispatch("LOAD_META_DATA_NEW")]);
                     return results;
                 } catch (e) {
                     console.log("Error loading data: " + e.message);
